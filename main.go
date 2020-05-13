b145fd63ae4f86010b6e3e9795fec9f9ce4b0ef3
georgihimchevinstana <55439477+georgihimchevinstana@users.noreply.github.com>
2020-05-13 16:22:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 func handler(w http.ResponseWriter, r *http.Request) {
         h, _ := os.Hostname()
-        fmt.Fprintf(w, "Yo, Yo ! Hi there guys and girls, I'm served from %s!", h)
+        fmt.Fprintf(w, "123123123Yo, Yo ! Hi there guys and girls, I'm served from %s!", h)
 }
 func main() {
         http.HandleFunc("/", handler)
